03c727c9f7cd616b2d4e3c312eb0ad0aa51df153
dheerajmandloi <dheerajmandloi.2005@gmail.com>
2026-09-04 09:04:39 +0530

    Initial commit

---
+++ src/main/java/com/example/career_education_advisor/CareerEducationAdvisorApplication.java
@@ -0,0 +1,13 @@
+package com.example.career_education_advisor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerEducationAdvisorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareerEducationAdvisorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/career_education_advisor/CareerEducationAdvisorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.career_education_advisor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerEducationAdvisorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
